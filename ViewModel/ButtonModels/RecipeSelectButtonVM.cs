78fc132a53b40d9f9e3c1ccb146a745e69e124c7
DerReiter <riderbcrich2@gmail.com>
2024-03-06 01:17:47 +0300

    Реализованы всплывающие подсказки для конопок с рецептами в селекторе. Улучшена разметка селектора, добавлен заголовок.

--- ViewModel/ButtonModels/RecipeSelectButtonVM.cs
+++ ViewModel/ButtonModels/RecipeSelectButtonVM.cs
@@ -1,17 +1,18 @@
-﻿using Prism.Commands;
-using SatisfactoryProductionManager.Model.Elements;
+﻿using SatisfactoryProductionManager.Model.Elements;
 using System;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace SatisfactoryProductionManager.ViewModel.ButtonModels
 {
     public class RecipeSelectButtonVM : ObjectButtonVM<Recipe>
     {
+        public RecipeTooltipVM Tooltip { get; }
+
         public RecipeSelectButtonVM(Recipe recipe)
         {
             InnerObject = recipe;
             ImageSource = new BitmapImage(new Uri($"../Assets/Resources/{recipe.Product.Resource}.png", UriKind.Relative));
+            Tooltip = new RecipeTooltipVM(recipe);
         }
     }
 }
---
+++ ViewModel/ButtonModels/RecipeTooltipVM.cs
@@ -0,0 +1,52 @@
+﻿using SatisfactoryProductionManager.Model.Elements;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SatisfactoryProductionManager.ViewModel.ButtonModels
+{
+    public class RecipeTooltipVM
+    {
+        public string Title { get; }
+        public ImageTuple[] Outputs { get; }
+        public ImageTuple[] Inputs { get; }
+
+
+        public RecipeTooltipVM(Recipe recipe)
+        {
+            Title = recipe.Name;
+
+            if (recipe.HasByproduct)
+            {
+                Outputs = 
+                    [
+                        new ImageTuple(recipe.Product.Resource, recipe.Product.CountPerMinute),
+                        new ImageTuple(recipe.Byproduct.Value.Resource, recipe.Byproduct.Value.CountPerMinute)
+                    ];
+            }
+            else
+            {
+                Outputs = [new ImageTuple(recipe.Product.Resource, recipe.Product.CountPerMinute)];
+            }
+
+            Inputs = recipe.Inputs
+                .Select(stream =>new ImageTuple(stream.Resource, stream.CountPerMinute)).ToArray();
+        }
+
+
+        public struct ImageTuple
+        {
+            public ImageSource Image { get; }
+            public string Count { get; }
+
+            public ImageTuple(string resource, double count)
+            {
+                Image = new BitmapImage(new Uri ($"../Assets/Resources/{resource}.png", UriKind.Relative));
+                Count = count.ToString("0.###", CultureInfo.InvariantCulture) + "/мин.";
+            }
+        }
+    }
+}
